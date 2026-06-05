9779637efc3ac371326a5d1b396fa40cdc23edb3
Katchy <katchypotato@gmail.com>
2026-06-05 15:03:52 -0700

    Added weapons section, updated cursor

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-// have cursor gif follow  mouse
+// have cursor gif follow mouse
 const cursor = document.getElementById("cursor")
 
 document.addEventListener("mousemove", (e) => {
     cursor.style.left = (e.clientX - cursor.width / 2) + 'px';
     cursor.style.top = (e.clientY - cursor.height / 2) + 'px';
 })
+
+// switch to pointer for hovering
+document.querySelectorAll(".hover").forEach(el => {
+    el.addEventListener("mouseenter", () => {
+        cursor.src = "images/pointer.gif";
+    });
+
+    el.addEventListener("mouseleave", () => {
+        cursor.src = "images/cursor.gif";
+    });
+});
